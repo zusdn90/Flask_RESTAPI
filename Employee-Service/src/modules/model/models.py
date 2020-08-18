b5cec0e4d90380b4433504c1e3de307279fd4609
DESKTOP-QBKRSAC\HONG <gusdn90@gmail.com>
2020-08-18 17:50:49 +0900

    ORM방식으로 테이블 생성 / 데이터 저장 샘플 API 구현

--- Employee-Service/src/modules/model/models.py
+++ Employee-Service/src/modules/model/models.py
@@ -1,24 +1,26 @@
 import datetime
 
-from server import app as mainApp
+from server import app
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column
 
-db = SQLAlchemy(app=mainApp)
+db = SQLAlchemy(app)
 
 # 사원 정보 테애블
 class Employee(db.Model):
+    __tablename__ = 'Employee'
+
     column = ['employee_code', 'employee_name','dept_code','rank_code', 'address', 'salary', 'phone_number', 'date']
-    employee_code = Column(db.String(50), primary_key=True)
-    employee_name = Column(db.String(50), nullable=False, default='')
-    dept_code = Column(db.String(50), nullable=False, default='')
-    rank_code = Column(db.String(50), nullable=False, default='')
-    address = Column(db.String(50), nullable=False, default='')
-    salary = Column(db.Integer, nullable=False, default=0)
-    date = Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    employee_code = db.Column(db.String(50), primary_key=True)
+    employee_name = db.Column(db.String(50), nullable=False, default='')
+    dept_code = db.Column(db.String(50), nullable=False, default='')
+    rank_code = db.Column(db.String(50), nullable=False, default='')
+    address = db.Column(db.String(50), nullable=False, default='')
+    salary = db.Column(db.Integer, nullable=False, default=0)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
 
 
-    def __init__(self, employee_name, dept_code, rank_code, address, salary, date):
+    def __init__(self, employee_code, employee_name, dept_code, rank_code, address, salary, date):
+        self.employee_code = employee_code
         self.employee_name = employee_name
         self.dept_code = dept_code
         self.rank_code = rank_code
@@ -28,18 +30,22 @@ class Employee(db.Model):
         
 # 부서 정보 테이블
 class Department(db.Model):
+    __tablename__ = 'Department'
+
     column = ['dept_code', 'dept_name']
-    dept_code = Column(db.String(50), primary_key=True)
-    dept_name = Column(db.String(50), nullable=False)
+    dept_code = db.Column(db.String(50), primary_key=True)
+    dept_name = db.Column(db.String(50), nullable=False)
 
     def __init__(self, dept_name):        
         self.dept_name = dept_name
 
 # 직급 정보 테이블
 class Rank(db.Model):
+    __tablename__ = 'Rank'
+
     column = ['rank_code', 'rank_name']
-    rank_code = Column(db.String(50), primary_key=True)
-    rank_name = Column(db.String(50), nullable=False)
+    rank_code = db.Column(db.String(50), primary_key=True)
+    rank_name = db.Column(db.String(50), nullable=False)
 
     def __init__(self, rank_name):
         self.rank_name = rank_name
@@ -47,5 +53,5 @@ class Rank(db.Model):
 
 if __name__ == "__main__":
     # 테이블 생성
-    db.create_all()
+    db.create_all()    
     
--- Employee-Service/src/server.py
+++ Employee-Service/src/server.py
@@ -6,6 +6,7 @@ import logging
 import time
 import requests
 
+from datetime import datetime as dt
 from flask import Flask, request, Blueprint, jsonify, make_response, render_template
 from flask_restplus import Api, Resource
 from flask_restful import reqparse
@@ -16,11 +17,12 @@ from modules.api.v01.ns_user import api as user_api
 from modules.api.v01.ns_database import api as database_api
 from modules.setting import dbConnector
 from modules.setting import log as logger
+from modules.model import models
 
 # ============================ init Flask config ============================
 app = Flask(__name__, static_folder='static', template_folder='template')
-app.config['SECRET_KEY'] = uuid.uuid1()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://matrix:matrix@localhost:3306/matrix'
+#app.config['SECRET_KEY'] = uuid.uuid1()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://matrix_hhw:matrix_hhw@192.168.0.50:3306/matrix_hhw'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 cors = CORS(app, resources={
@@ -52,6 +54,37 @@ def start_schedule():
         schedule.run_pending()
     time.sleep(1)
 
+@app.route('/select')
+def select():
+    row = models.Employee.query.all()
+    return str(row)
+
+@app.route('/dataInsert')
+def insert():
+    new_data = models.Employee(
+        employee_code='asjd345jdv',
+        employee_name= 'hwhong',
+        dept_code='aaa',
+        rank_code='jij',
+        address='seoul',
+        salary=1000,
+        date=dt.now()
+    )
+    models.db.session.add(new_data)
+    models.db.session.commit()
+
+    return "Insert Success..."
+
+@app.route('/dataDelete')
+def delete():
+    id = models.Employee(
+        employee_code='asjd345jdv'
+    )
+    models.db.session.delete(id)
+    models.db.session.commit()
+
+    return "Insert Success..."    
+
 def job():
     url = 'http://127.0.0.1:5002/api/v01/users'
     response = requests.get(url=url)
@@ -74,5 +107,13 @@ class HttpStatusResponse(Resource):
     def serverError(self):
         return "[500] Server Error..."
 
-if __name__ == '__main__':    
+if __name__ == '__main__':  
+    try:  
+        models.db.init_app(app)
+        # 테이블 삭제 후 생성
+        models.db.drop_all()
+        models.db.create_all()
+    except Exception as e:
+        logger.error_log(str(e))        
+
     app.run(host='127.0.0.1', port=5002, debug=True)
